b4d9772425bc7e3515f2140f51531f91c96ed512
Pham Viet Hung <pham0084@e.ntu.edu.sg>
2024-05-31 01:41:48 +0800

    feat: add user-agent endpoint

--- test.js
+++ test.js
@@ -14,6 +14,15 @@ tape('should respond hello', (t) => {
     t.end()
   })
 })
+tape('should respond user-agent', (t) => {
+  const opts = { headers: { 'User-Agent': 'tape' } }
+  jsonist.get(`${urlBase}/user-agent`, opts, (err, body) => {
+    if (err) t.error(err)
+
+    t.equal(body.ua, 'tape')
+    t.end()
+  })
+})
 
 tape('cleanup', function (t) {
   server.close()
